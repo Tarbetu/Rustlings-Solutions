420fe1199dc5762584d4682a74284921f3597cc0
Emrecan Şuşter <emrecansuster@tuta.io>
2021-12-13 13:03:53 +0300

    finished all the rustlings things

---
+++ clippy/target/debug/deps/clippy2-016147f6fbe419d0.d
@@ -0,0 +1,7 @@
+/home/emre/Genel/öğrenme/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-016147f6fbe419d0.rmeta: clippy2.rs
+
+/home/emre/Genel/öğrenme/rust/rustlings/exercises/clippy/target/debug/deps/clippy2-016147f6fbe419d0.d: clippy2.rs
+
+clippy2.rs:
+
+# env-dep:CLIPPY_ARGS=-D__CLIPPY_HACKERY__warnings__CLIPPY_HACKERY__
---
+++ functions/functions1.rs
@@ -0,0 +1,11 @@
+// functions1.rs
+// Make me compile! Execute `rustlings hint functions1` for hints :)
+
+
+fn call_me() {
+    println!("Hala daha aynıyız :d");
+}
+
+fn main() {
+    call_me();
+}
